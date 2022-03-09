b5ddd5e3a80cfce3ad38e0a3cd93188b8c5e3d7f
auliaaepa <auliaepa@gmail.com>
2022-03-10 00:00:06 +0700

    fix serversocket

--- client/client_serversocket.py
+++ client/client_serversocket.py
@@ -1,8 +1,10 @@
 import socket
 import sys
 
-# define buffer size
+# define buffer size, encoding format, and separator for header message
 BUF_SIZE = 1024
+FORMAT = "utf-8"
+SEPARATOR = b",\n"
 
 # initialize socket object with AF_INET as address family and SOCK_STREAM as socket type
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,26 +21,33 @@ try:
     while True:
         # request to server
         message = sys.stdin.readline()
-        client_socket.send(bytes(message, 'utf-8'))
+        client_socket.send(bytes(message, FORMAT))
 
         # receive response from server
-        received_msg = client_socket.recv(BUF_SIZE).decode('utf-8')
+        received_msg = client_socket.recv(BUF_SIZE).decode(FORMAT)
         sys.stdout.write(received_msg)
         # download file if command correct
         if received_msg == "Start sending file...\n":
-            # get file name and file size
-            file_name = client_socket.recv(BUF_SIZE).decode('utf-8')
-            file_size = int(client_socket.recv(BUF_SIZE).decode('utf-8'))
+            # extract header(file name and file size) and content from message
+            received_file = client_socket.recv(BUF_SIZE)
+            extract = received_file.split(SEPARATOR, 2)
+            file_name, file_size, content = extract[0].decode(FORMAT), int(extract[1].decode(FORMAT)), extract[2]
             new_file_name = "from_server_" + file_name
+
             # get file content
             with open(new_file_name, "wb") as file:
-                received_size = 0
+                file.write(content)
+                content_size = len(content)
+                sys.stdout.write(">> {:>6.2f}%\n".format(content_size*100/file_size))
                 # loop until get full file content
-                while received_size < file_size:
+                while content_size < file_size:                    
                     received_file = client_socket.recv(BUF_SIZE)
-                    received_size += BUF_SIZE
-                    file.write(received_file)
-                file.close()
+                    extract = received_file.split(SEPARATOR, 2)
+                    content = extract[2]
+                    file.write(content)
+                    content_size += len(content)
+                    sys.stdout.write(">> {:>6.2f}%\n".format(content_size*100/file_size))
+
             sys.stdout.write(">> " + file_name + " successfully downloaded.\n")
             sys.stdout.write(">> file saved as " + new_file_name + "\n")
         sys.stdout.write(">> ")
--- server/server_serversocket.py
+++ server/server_serversocket.py
@@ -1,144 +1,97 @@
-#!/usr/bin/env python
-
-import socket
+import os 
+import threading 
+import socketserver 
 import sys
-import argparse
 
-host = 'localhost'
-SEND_BUF_SIZE = 4096
-RECV_BUF_SIZE = 4096
+# define buffer size, encoding format, and separator for header message
+BUF_SIZE = 1024
+FORMAT = "utf-8"
+SEPARATOR = ",\n"
 
-def modify_buff_size():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Get the size of the socket's send buffer
-    bufsize = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-    print ("Buffer size [Before]:%d" %bufsize)
-    sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, SEND_BUF_SIZE)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, RECV_BUF_SIZE)
-    bufsize = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-    print ("Buffer size [After]:%d" %bufsize)
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler): 
+    def handle(self):
+        print(threading.current_thread().name, "Accepted client:", self.request.getpeername())
+        while True:
+            try:
+                # receive message from client
+                message = self.request.recv(BUF_SIZE).decode(FORMAT)
+                if message:
+                    # get command
+                    splitted_message = message.split()
+                    command = splitted_message[0]
 
-if __name__ == '__main__':
-    modify_buff_size()
+                    if command == "unduh":
+                        if len(splitted_message) != 2: 
+                            error_msg = "Unknown argument\n"
+                            self.request.send(error_msg.encode(FORMAT))
+                            print(threading.current_thread().name, "Send to client :", self.request.getpeername(), error_msg.encode(FORMAT))
+                            continue
+                        
+                        # get filename and filepath
+                        filename = splitted_message[1]
+                        filepath = os.path.join(os.getcwd(), "dataset", filename)
 
-def echo_client(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # Connect the socket to the server     
-    server_address = (host, port)     
-    print ("Connecting to %s port %s" % server_address)     
-    sock.connect(server_address)     
-    # Send data     
-    try:         
-        # Send data         
-        message = "Test message. This will be echoed"         
-        print ("Sending %s" % message)         
-        sock.sendall(message.encode('utf-8'))         # Look for the response         
-        amount_received = 0         
-        amount_expected = len(message)         
-        while amount_received < amount_expected:             
-            data = sock.recv(16)             
-            amount_received += len(data)             
-            print ("Received: %s" % data)     
-    except socket.error as e:         
-        print ("Socket error: %s" %str(e))     
-    except Exception as e:         
-        print ("Other exception: %s" %str(e))     
-    finally:        
-        print ("Closing connection to the server")         
-        sock.close()
-
-if __name__ == '__main__':     
-    parser = argparse.ArgumentParser(description='Socket Server Example')     
-    parser.add_argument('--port', action="store", dest="port", type=int, required=True)     
-    given_args = parser.parse_args()     
-    port = given_args.port     
-    echo_client(port)
-
-
-import socket
-import select
-import sys
-import os
-
-# initialize socket object with AF_INET as address family and SOCK_STREAM as socket type
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# validating address passed to bind() must allow reuse of local address
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# bind to the given address
-HOST = str(sys.argv[1])
-PORT = int(sys.argv[2])
-server_address = (HOST, PORT)
-server_socket.bind(server_address)
-
-# accept connections from 5 clients
-server_socket.listen(5)
-
-# list to store accepted client
-input_socket = [server_socket]
-
-try:
-    while True:
-        # get read, write, and exception list from select
-        read_list, write_list, exception_list = select.select(input_socket, [], [])
-        
-        for sock in read_list:
-            # accept client and add it to the accepted client list
-            if sock == server_socket:
-                client_socket, client_address = server_socket.accept()
-                input_socket.append(client_socket)
-                print ("Accepted client:", client_address)     
-            
-            # handle receiving message and sending file
-            else:
-                try:        	
-                    message = sock.recv(1024).decode("utf-8")
-                    splited_message = message.split()
-                    command, filename = splited_message[0], splited_message[1]
-                    splited_filename = filename.split(".")
-                    name, ext = splited_filename[0], splited_filename[1]
-                    
-                    filepath = "dataset/" + filename
-                    # print(message); print(command); print(filename); print(name); print(ext)
-                    
-                    if message:
-                        if command == "unduh":
-                            if os.path.exists(filepath): 
-                                success_msg = "Start sending file...\n"
-                                sock.send(success_msg.encode("utf-8"))
-                                print("Send to client :", client_address, success_msg.encode("utf-8"))
-                                
-                                sock.send(filename.encode("utf-8"))
-                                sock.send(str(os.path.getsize(filepath)).encode("utf-8"))
-
-                                if ext == "html" or ext == "txt": 
-                                    with open(filepath, 'rb') as file:
-                                        content = file.read()
-                                        sock.sendall(content)
-                                        print("File sent")
-                                        file.close()
-                            else:
-                                error_msg = "File not found\n"
-                                sock.send(error_msg.encode("utf-8"))
-                                print("Send to client :", client_address, error_msg.encode("utf-8"))                           
+                        # if requested file exist in dataset
+                        if os.path.exists(filepath):
+                            success_msg = "Start sending file...\n"
+                            self.request.send(success_msg.encode(FORMAT))
+                            print(threading.current_thread().name, "Send to client :", self.request.getpeername(), success_msg.encode(FORMAT))
+                            
+                            # create header message
+                            filesize = os.path.getsize(filepath)
+                            header = (filename + SEPARATOR + str(filesize) + SEPARATOR).encode(FORMAT)
+                            
+                            # send file content
+                            with open(filepath, 'rb') as file:
+                                send_size = 0
+                                while send_size < filesize:
+                                    content = file.read(BUF_SIZE-len(header))
+                                    send_file = header + content
+                                    self.request.send(send_file)
+                                    send_size += len(content)
+                                    print(threading.current_thread().name, "Send to client :", self.request.getpeername(), filename, "[{:>6.2f}%]".format(send_size*100/filesize))
                         else:
-                            error_msg = "Unknown command\n"
-                            sock.send(error_msg.encode("utf-8"))
-                            print("Send to client :", client_address, error_msg.encode("utf-8"))
-                    else:                    
-                        sock.close()
-                        input_socket.remove(sock)
-                
-                except IndexError:
-                    error_msg = "Unknown command\n"
-                    sock.send(error_msg.encode("utf-8"))
-                    print("Send to client :", client_address, error_msg.encode("utf-8"))
-                
-                except FileNotFoundError:
-                    error_msg = "File not found\n"
-                    sock.send(error_msg.encode("utf-8"))
-                    print("Send to client :", client_address, error_msg.encode("utf-8"))
+                            error_msg = "File not found\n"
+                            self.request.send(error_msg.encode(FORMAT))
+                            print(threading.current_thread().name, "Send to client :", self.request.getpeername(), error_msg.encode(FORMAT))
+                    else:
+                        error_msg = "Unknown command\n"
+                        self.request.send(error_msg.encode(FORMAT))
+                        print(threading.current_thread().name, "Send to client :", self.request.getpeername(), error_msg.encode(FORMAT))
+                # client close connection
+                else:
+                    print(threading.current_thread().name, "Closed client  :", self.request.getpeername())
+                    self.request.close()
+                    break
+            
+            except IndexError:
+                error_msg = "Unknown command\n"
+                self.request.send(error_msg.encode(FORMAT))
+                print(threading.current_thread().name, "Send to client :", self.request.getpeername(), error_msg.encode(FORMAT))            
 
-except KeyboardInterrupt:        
-    server_socket.close()
-    sys.exit(0)
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    """Nothing to add here, inherited everything necessary from parents"""
+    pass
+
+if __name__ == "__main__":
+    # set server address from the given address
+    HOST = str(sys.argv[1])
+    PORT = int(sys.argv[2])
+    server_address = (HOST, PORT)
+
+    # initialize socket server
+    socket_server = ThreadedTCPServer(server_address, ThreadedTCPRequestHandler)
+
+    try: 
+        # start a thread with the socket server - will create one thread for each request
+        server_thread = threading.Thread(target=socket_server.serve_forever)
+        # server_thread.daemon = False
+        server_thread.start()
+        print(server_thread.name, "Server bind to :", socket_server.server_address)
+
+        # socket_server.serve_forever()
+
+    except KeyboardInterrupt:
+        # socket_server.shutdown()
+        socket_server.server_close()
+        sys.exit(0)
